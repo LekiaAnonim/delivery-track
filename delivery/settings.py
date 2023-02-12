5e0cb96a89750d748f876051fbc6bcf005b2e1f5
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2023-02-12 01:45:36 +0100

    add STATICFILES_DIRS and STATICFILES_FINDERS to settings.py

--- delivery/settings.py
+++ delivery/settings.py
@@ -117,6 +117,11 @@ MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "track_delivery/static"),)
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 
 # Password validation
